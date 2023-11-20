cfbde4aa5cf51d2d0549c7f5dd3e44af9278cd4e
Charles Schleich <schleich.cdaniel@gmail.com>
2023-11-20 11:35:10 +0100

    WIP: Writing as Javascript as .mjs file

--- zenoh-wasm/build-commands.sh
+++ zenoh-wasm/build-commands.sh
@@ -6,7 +6,7 @@ ls
 cd build
 echo "building from $(pwd)"
 CMAKE_EXPORT_COMPILE_COMMANDS=1 emcmake cmake ..
-make all
+make -j10 all # gotta go fast
 cd ..
 chmod -R a+rw build.emscripten
 chmod -R a+rw build
